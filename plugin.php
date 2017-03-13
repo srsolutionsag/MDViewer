d55307e8812194d9b639c719ebb8a988f98a6abc
Fabian Schmid <fs@studer-raimann.ch>
2017-03-13 18:13:51 +0100

    ILIAS-versions

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'md_tme';
 $version = '1.0.0';
-$ilias_min_version = '5.2.0';
+$ilias_min_version = '5.1.0';
 $ilias_max_version = '5.2.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'support@studer-raimann.ch';
