00c88560814fedd8ad994f42f3bd48a594ace512
Fabian Schmid <fs@studer-raimann.ch>
2018-03-27 13:47:13 +0200

    updated library

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,20 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d
 {
-    public static $prefixesPsr0 = array (
+    public static $prefixLengthsPsr4 = array (
         'M' => 
         array (
-            'Michelf' => 
-            array (
-                0 => __DIR__ . '/..' . '/michelf/php-markdown',
-            ),
+            'Michelf\\' => 8,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Michelf\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/michelf/php-markdown/Michelf',
         ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixesPsr0 = ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d::$prefixesPsr0;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
