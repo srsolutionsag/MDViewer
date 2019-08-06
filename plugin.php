3dc8d92e76ed97d0f8451d7c5111100205462187
Lukas Zehnder <lz@studer-raimann.ch>
2019-08-06 07:45:40 +0200

    compatibility-update for ILIAS v5.4

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'md_tme';
 $version = '1.1.0';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'support@studer-raimann.ch';
 
