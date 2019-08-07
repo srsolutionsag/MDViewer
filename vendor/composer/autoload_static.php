71e6a5e54131ed861a214fb584280badf74b6b65
Lukas Zehnder <lz@studer-raimann.ch>
2019-08-07 07:17:16 +0200

    created plugin configuration (settings can be saved, but don't get used yet)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,19 @@ class ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d
         ),
     );
 
+    public static $classMap = array (
+        'ilMDViewerConfig' => __DIR__ . '/../..' . '/classes/class.ilMDViewerConfig.php',
+        'ilMDViewerConfigGUI' => __DIR__ . '/../..' . '/classes/class.ilMDViewerConfigGUI.php',
+        'ilMDViewerPlugin' => __DIR__ . '/../..' . '/classes/class.ilMDViewerPlugin.php',
+        'ilMDViewerPluginGUI' => __DIR__ . '/../..' . '/classes/class.ilMDViewerPluginGUI.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit20452b739bfc4e5bdd6fb4068763612d::$classMap;
 
         }, null, ClassLoader::class);
     }
